d10f39858183c7c58689a057edcf56301a966f50
Andressa Valengo <valengoandressa@gmail.com>
2020-10-10 14:41:23 -0300

    organized inital files

---
+++ Gethack/Gethack/Supporting/GethackApp.swift
@@ -0,0 +1,17 @@
+//
+//  GethackApp.swift
+//  Gethack
+//
+//  Created by Andressa Valengo on 10/10/20.
+//
+
+import SwiftUI
+
+@main
+struct GethackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gethack/Gethack/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gethack
+//
+//  Created by Andressa Valengo on 10/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
