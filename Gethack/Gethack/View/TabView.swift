4c40b432fa353302af45196bad017e03a6f3c72d
Eduarda Mello <duda.mello1@hotmail.com>
2020-10-11 09:30:13 -0300

    refactoring

---
+++ Gethack/Gethack/View/TabView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gethack
+//
+//  Created by Eduarda Mello on 11/10/20.
+//
+
+//import SwiftUI
+//
+//struct TabView: View {
+//    
+//    var body: some View {
+//        Text("oi")
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TabView()
+//    }
+//}
